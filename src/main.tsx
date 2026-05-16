96216d056fbf934d493d4dd481d49204199af144
Eric Villeda <ericjared29@gmail.com>
2026-05-15 22:08:12 -0600

    add navbar, fonts and logo

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,13 @@
+import '@fontsource/source-sans-pro/300.css';
+import '@fontsource/source-sans-pro/400.css';
+import '@fontsource/source-sans-pro/600.css';
+import '@fontsource/source-sans-pro/700.css';
+
+import '@fontsource/montserrat/300.css';
+import '@fontsource/montserrat/400.css';
+import '@fontsource/montserrat/600.css';
+import '@fontsource/montserrat/700.css';
+
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
