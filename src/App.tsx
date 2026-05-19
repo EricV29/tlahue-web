ce45356f65dd6c2e36202e3431daa2f5b8181ced
Eric Villeda <ericjared29@gmail.com>
2026-05-18 21:34:43 -0600

    complete events section

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,23 @@
-import Navbar from './components/Navbar'
-import Hero from './components/Hero'
-import Novedades from './components/Novedades'
-import Gobierno from './components/Gobierno'
-import FloatingSocial from './components/FloatingSocial'
-import FloatingEmergency from './components/FloatingEmergency'
+import Navbar from "./components/Navbar";
+import Hero from "./components/Hero";
+import Events from "./components/Events";
+import Gobierno from "./components/Gobierno";
+import FloatingSocial from "./components/FloatingSocial";
+import FloatingEmergency from "./components/FloatingEmergency";
 
 function App() {
   return (
-    <div className="min-h-screen bg-[#ffffff]">
+    <div className="min-h-screen bg-canvas-white">
       <Navbar />
       <main>
         <Hero />
-        <Novedades />
+        <Events />
         <Gobierno />
       </main>
       <FloatingSocial />
       <FloatingEmergency />
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
